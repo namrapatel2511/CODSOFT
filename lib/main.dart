df460b105b16fc6ebc4441e9cbc113e2b5b0acad
namrapatel2511 <21it115@charusat.edu.in>
2024-06-01 19:13:31 +0530

    full_app

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,6 @@ class _MyTasksPageState extends State<MyTasksPage> {
       ),
       body: Stack(
         children: [
-          // Gradient background
           Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
@@ -72,7 +71,6 @@ class _MyTasksPageState extends State<MyTasksPage> {
               ),
             ),
           ),
-          // Blurred effect
           ImageFiltered(
             imageFilter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
             child: Container(
@@ -111,7 +109,7 @@ class _MyTasksPageState extends State<MyTasksPage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: _showAddTaskDialog,
         backgroundColor: const Color.fromARGB(255, 233, 233, 233),
         child: const Icon(Icons.add),
       ),
@@ -131,6 +129,22 @@ class _MyTasksPageState extends State<MyTasksPage> {
               (task) => !task.isCompleted && task.date.isBefore(DateTime.now()))
           .toList();
     }
+  }
+
+  void _showAddTaskDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AddTaskDialog(
+          onTaskAdded: (task) async {
+            setState(() {
+              tasks.add(task);
+            });
+            await _saveTasks();
+          },
+        );
+      },
+    );
   }
 }
 
@@ -155,47 +169,6 @@ class Task {
       'date': date.toIso8601String(),
       'isCompleted': isCompleted,
     };
-  }
-}
-
-class TaskFilter extends StatelessWidget {
-  final Function(String) onFilterChanged;
-
-  TaskFilter({required this.onFilterChanged});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          _buildFilterChip('All', Icons.filter_list),
-          _buildFilterChip('Pending', Icons.access_time),
-          _buildFilterChip('Complete', Icons.done),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildFilterChip(String label, IconData icon) {
-    return FilterChip(
-      label: Row(
-        children: [
-          Icon(icon, size: 22),
-          const SizedBox(width: 10),
-          Text(
-            label,
-            style: TextStyle(fontSize: 18),
-          ),
-        ],
-      ),
-      onSelected: (isSelected) {
-        onFilterChanged(label);
-      },
-      backgroundColor: Colors.white,
-      selectedColor: Colors.amber[800],
-    );
   }
 }
 
@@ -263,6 +236,10 @@ class TaskList extends StatelessWidget {
                       },
                     ),
                   IconButton(
+                    icon: const Icon(Icons.edit, color: Colors.white),
+                    onPressed: () => _showEditTaskDialog(context, task),
+                  ),
+                  IconButton(
                     icon: const Icon(Icons.delete, color: Colors.white),
                     onPressed: () async {
                       tasks.remove(task);
@@ -275,6 +252,182 @@ class TaskList extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+
+  void _showEditTaskDialog(BuildContext context, Task task) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AddTaskDialog(
+          task: task,
+          onTaskUpdated: (updatedTask) async {
+            task.title = updatedTask.title;
+            task.date = updatedTask.date;
+            task.isCompleted = updatedTask.isCompleted;
+            onUpdate();
+          },
+        );
+      },
+    );
+  }
+}
+
+class TaskFilter extends StatelessWidget {
+  final Function(String) onFilterChanged;
+
+  TaskFilter({required this.onFilterChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildFilterChip('All', Icons.filter_list),
+          _buildFilterChip('Pending', Icons.access_time),
+          _buildFilterChip('Complete', Icons.done),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFilterChip(String label, IconData icon) {
+    return FilterChip(
+      label: Row(
+        children: [
+          Icon(icon, size: 22),
+          const SizedBox(width: 10),
+          Text(
+            label,
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
+      ),
+      onSelected: (isSelected) {
+        onFilterChanged(label);
+      },
+      backgroundColor: Colors.white,
+      selectedColor: Colors.amber[800],
+    );
+  }
+}
+
+class AddTaskDialog extends StatefulWidget {
+  final Function(Task)? onTaskAdded;
+  final Function(Task)? onTaskUpdated;
+  final Task? task;
+
+  AddTaskDialog({this.onTaskAdded, this.onTaskUpdated, this.task});
+
+  @override
+  _AddTaskDialogState createState() => _AddTaskDialogState();
+}
+
+class _AddTaskDialogState extends State<AddTaskDialog> {
+  final _formKey = GlobalKey<FormState>();
+  late String _taskTitle;
+  late DateTime _taskDate;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.task != null) {
+      _taskTitle = widget.task!.title;
+      _taskDate = widget.task!.date;
+    } else {
+      _taskTitle = '';
+      _taskDate = DateTime.now();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: const Color(0xFF4E342E),
+      title: Text(widget.task == null ? 'Add Task' : 'Edit Task',
+          style: const TextStyle(color: Colors.white)),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              initialValue: _taskTitle,
+              decoration: const InputDecoration(
+                labelText: 'Task Title',
+                labelStyle: TextStyle(color: Colors.white),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a task title';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _taskTitle = value!;
+              },
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 8.0),
+            TextFormField(
+              readOnly: true,
+              decoration: const InputDecoration(
+                labelText: 'Date',
+                labelStyle: TextStyle(color: Colors.white),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+              ),
+              onTap: () async {
+                DateTime? picked = await showDatePicker(
+                  context: context,
+                  initialDate: _taskDate,
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(2101),
+                );
+                if (picked != null) {
+                  setState(() {
+                    _taskDate = picked;
+                  });
+                }
+              },
+              controller: TextEditingController(
+                text: DateFormat('MMMM d, yyyy').format(_taskDate),
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancel', style: TextStyle(color: Colors.white)),
+        ),
+        TextButton(
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              _formKey.currentState!.save();
+              if (widget.onTaskAdded != null) {
+                widget.onTaskAdded!(Task(_taskTitle, _taskDate, false));
+              } else if (widget.onTaskUpdated != null) {
+                widget.onTaskUpdated!(
+                    Task(_taskTitle, _taskDate, widget.task!.isCompleted));
+              }
+              Navigator.of(context).pop();
+            }
+          },
+          child: Text(widget.task == null ? 'Add' : 'Update',
+              style: const TextStyle(color: Colors.white)),
+        ),
+      ],
     );
   }
 }
